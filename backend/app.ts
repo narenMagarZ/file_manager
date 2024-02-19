bb6bda45fb80dc705f06208b6137f9575e2ded53
narenMagarZ <narenmagarz98@gmail.com>
2024-02-19 07:47:50 +0545

    Added the cors

--- backend/app.ts
+++ backend/app.ts
@@ -1,6 +1,13 @@
 import express from 'express'
 import { router } from './routes'
-
+import cors from 'cors'
 export const app = express()
 
-app.use('/',router)
+app.use(cors({
+    origin:"http://localhost:3000",
+    optionsSuccessStatus:200,
+    credentials:true
+}))
+app.use(express.json())
+app.use(express.urlencoded({extended:false}))
+app.use('/api',router)
