5bb725f544193dad41967c15e17628c5dd756ded
narenMagarZ <narenmagarz98@gmail.com>
2024-02-23 21:14:39 +0545

    Separated the login form from page and check the user session to decide whether to render the page or redirect to home page

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -1,39 +1,19 @@
-"use client"
 
-import { InputBox } from "../[component]/input-box"
-import { FormButton } from "../[component]/form-btn"
 import Link from "next/link"
-import { FormEvent, useState } from "react"
-function Signin(){
-
-    const [email,setEmail] = useState("")
-    const [password,setPassword]=useState("")
-    const [emailErrorMsg,setEmailErrorMsg]=useState("")
-    const [passwordErrorMsg,setPasswordErrorMsg]=useState("")
-
-    function handleFormSubmission(e:FormEvent){
-        e.preventDefault()
-    }
+import Form from "./form"
+import { getServerSession } from "next-auth"
+import { redirect } from "next/navigation"
+async function Signin(){
+    const session = await getServerSession()
+    if(session)  redirect("/home")
     return(
         <main className="flex justify-center min-h-screen items-center bg-white">
             <div className="rounded p-4 bg-gray-100 max-w-sm">
                 <h1 className="text-center text-xl">Signin</h1>
-                <form onSubmit={handleFormSubmission}> 
-                    <div className="py-2">
-                        <InputBox label="Email" setValue={setEmail} errorMsg={emailErrorMsg} value={email} setErrorMsg={setEmailErrorMsg} />
-                        <div className="py-2"></div>
-                        <InputBox label="Password" setValue={setPassword} errorMsg={passwordErrorMsg} value={password} setErrorMsg={setPasswordErrorMsg} />
-                    </div>
-                    <div className="py-2"></div>
-                    <FormButton label="Signin"/>
-                </form>
-                <div className="text-center py-2">
-                    <span>OR</span>
-                </div>
-                <div></div>
+                <Form/>
                 <div className="text-center">
                     <p>Don't have an account? 
-                        <Link href={'/signup'}> Signup</Link>
+                        <Link className="hover:text-blue-500" href={'/signup'}> Signup</Link>
                     </p>
                 </div>
             </div>
