460090e1ada1bbdf63fa6ecec1d30d28152c5525
narenMagarZ <narenmagarz98@gmail.com>
2024-02-23 21:06:37 +0545

    Added routes for user and auth

--- backend/routes/index.ts
+++ backend/routes/index.ts
@@ -1,6 +1,7 @@
 import express from 'express'
-
+import { authRouter } from './auth-routes'
+import { userRouter } from './user-routes'
 export const router = express.Router()
 
-
-router.get('/')
+router.use('/user',userRouter)
+router.use('/auth',authRouter)
