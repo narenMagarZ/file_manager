e90392411973607871826698853b0f7942c8cd9e
narenMagarZ <narenmagarz98@gmail.com>
2024-02-23 21:08:50 +0545

    Added custom app error class

---
+++ backend/helpers/app-error.ts
@@ -0,0 +1,14 @@
+
+
+class AppError extends Error{
+    message:string
+    status:number
+    constructor(message:string,status:number){
+        super(message)
+        this.message = message
+        this.status = status
+    }
+}
+
+
+export default  AppError
