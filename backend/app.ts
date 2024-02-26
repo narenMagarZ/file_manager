e35201c69a91b25fc2ba609e3dcdf75892fa2a08
narenMagarZ <narenmagarz98@gmail.com>
2024-02-26 21:41:19 +0545

    Added cookie parser

--- backend/app.ts
+++ backend/app.ts
@@ -2,8 +2,10 @@ import express, { NextFunction,Request,Response } from 'express'
 import { router } from './routes'
 import cors from 'cors'
 import AppError from './helpers/app-error'
+import cookieParser from 'cookie-parser'
 export const app = express()
 
+app.use(cookieParser())
 app.use(cors({
     origin:"http://localhost:3000",
     optionsSuccessStatus:200,
